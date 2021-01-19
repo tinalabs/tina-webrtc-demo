b6654283720f359efa0edb7c2ad8270c92861fbe
Logan Anderson <landerson2@upei.ca>
2021-01-19 16:21:05 -0400

    signal server is working

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,23 @@ import { useForm, usePlugin } from "tinacms";
 import Layout from "../components/Layout";
 import SimplePeer from "simple-peer";
 import { useEffect, useMemo, useState } from "react";
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
 type Fields = {
   title: string;
   subtitle: string;
@@ -58,6 +75,14 @@ const IndexPage = () => {
       return p;
     }
   }, []);
+  const secondPeer = useMemo(() => {
+    // this doesnt work in SSR
+    if (typeof window == "undefined") {
+      return (undefined as unknown) as boolean;
+    } else {
+      return location.hash === "#1";
+    }
+  }, []);
 
   useEffect(() => {
     p.on("error", (err) => console.log("error", err));
@@ -65,6 +90,13 @@ const IndexPage = () => {
     p.on("signal", (data) => {
       console.log("SIGNAL", JSON.stringify(data));
       setSignal(JSON.stringify(data));
+      if (data.type === "offer") {
+        console.log("posting");
+        postData("/api/signal?slug=/", { data, type: "offer" });
+      } else if (data.type == "answer") {
+        console.log("posting awnser");
+        postData("/api/signal?slug=/", { data, type: "answer" });
+      }
     });
 
     p.on("connect", () => {
@@ -76,6 +108,24 @@ const IndexPage = () => {
       console.log("data: " + data);
       setFormState(JSON.parse(data));
     });
+    const fetchOffer = async () => {
+      if (secondPeer) {
+        const res = await fetch("/api/signal?slug=/");
+        const { offer } = await res.json();
+        if (offer) {
+          p.signal(offer);
+        }
+      } else {
+        setTimeout(async () => {
+          const res = await fetch("/api/signal?slug=/");
+          const { answer } = await res.json();
+          if (answer) {
+            p.signal(answer);
+          }
+        }, 10000);
+      }
+    };
+    fetchOffer();
   }, []);
 
   // this is bad
